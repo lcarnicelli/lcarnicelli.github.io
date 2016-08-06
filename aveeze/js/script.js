9bbb6ffe0f2a3263f531750b00e45620dd31cb77
lcarnicelli <lucas.carnicelli@gmail.com>
2016-08-06 00:41:30 -0300

    Update script.js

--- aveeze/js/script.js
+++ aveeze/js/script.js
@@ -1,23 +1,25 @@
 (function($) {
-	// Menu
-	$('#menu-icon').click(function(e) {
-		e.preventDefault();
-		
-		$('#menu').slideToggle(200).toggleClass('opened');
-	});
+    // Menu
+    $('#menu-icon').click(function(e) {
+        e.preventDefault();
+        
+        $('#menu').slideToggle(200).toggleClass('opened');
+    });
 
-	// ScrollTo
-	$('[data-bind="scroll-to"]').click(function(e) {
-		e.preventDefault();
-		
-		var element = $(this).data('to');
+    // ScrollTo
+    $('[data-bind="scroll-to"]').click(function(e) {
+        e.preventDefault();
+        
+        var element = $(this).data('to');
 
-		if (element) {
-			$('html, body').animate({
-		        scrollTop: $(element).offset().top
-		    }, 800);
+        if (element) {
+            var bodyPaddingTop = parseInt($('body').css('padding-top'));
 
-		    $('#menu.opened').removeClass('opened').hide();
-		}
-	});
+            $('html, body').animate({
+                scrollTop: $(element).offset().top - bodyPaddingTop
+            }, 800);
+
+            $('#menu.opened').removeClass('opened').hide();
+        }
+    });
 })(jQuery);
