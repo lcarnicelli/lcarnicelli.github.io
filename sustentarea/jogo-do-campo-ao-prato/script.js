dad18df4559d69df4200afe9a67867919b39335b
lcarnicelli <lucas.carnicelli@gmail.com>
2025-08-17 21:20:34 -0300

    Update script.js

--- sustentarea/jogo-do-campo-ao-prato/script.js
+++ sustentarea/jogo-do-campo-ao-prato/script.js
@@ -3,36 +3,70 @@ window.onload = (e) => {
 		{
 			'id': 1,
 			'category': '',
-			'title': 'Card 1',
-			'excerpt': 'Esse é o resumo do primeiro cartão, o texto que está impresso nele.',
-			'image': '',
-			'text': '',
+			'title': 'Refeição com alimentos ultraprocessados',
+			'excerpt': 'Some quick example text to build on the card title and make up the bulk of the card’s content.',
+			'image': 'https://www.daybreakgame.org/assets/dyb_local_project_cards_pt-br_v1.00_03.png',
+			'text': '<p>A nova classificação de alimentos, chamada classificação NOVA, divide os alimentos em 4 grupos, sendo eles: </p><p>Grupo 1- Alimentos in natura ou minimamente processados: é aquele que temos acesso da forma que veio da natureza, incluindo partes comestíveis de plantas (como sementes, frutas, folhas, raízes) ou de animais (músculos, ovos, leite), bem como cogumelos e algas. Já os minimamente processados, são alimentos in natura que precisam de algum processamento para chegar ao consumidor, o que inclui o uso de embalagens, congelamento, pasteurização, moagem, dentre outros que não modificam suas propriedades, apenas aumentam a durabilidade e facilitam o consumo. </p><p>Grupo 2- Ingredientes culinários processados: são ingredientes essenciais para transformar os alimentos do grupo 1 em receitas e refeições, como sal, óleos e açúcar, e que, quando usados em pequenas quantidades, são bem-vindos em uma alimentação nutricionalmente equilibrada.</p><p>Grupo 3- Alimentos processados: são alimentos do grupo 1 que passaram por adição de ingredientes do grupo 2, sendo esse processo feito industrialmente ou domesticamente. Isso aumenta a durabilidade dos alimentos e, quando consumidos com moderação em um padrão alimentar baseado majoritariamente em alimentos do grupo 1, contribuem para uma alimentação equilibrada nutricionalmente e saudável. </p><p>Grupo 4- Alimentos ultraprocessados: são formulações industriais que modificam a matriz de um alimento in natura, de modo que nem se parecem com eles. Em geral, contém muitos ingredientes, incluindo corantes, aromatizantes, espessantes e emulsificantes, que conferem hiperpalatabilidade (sabor intenso) e longa duração a esses produtos. Eles são desenvolvidos visando o lucro e substituição dos alimentos dos grupos anteriores, sendo uma ameaça à cultura, ao meio ambiente e à saúde da população. Isso ocorre devido ao distanciamento da terra e dos alimentos em sua forma natural, redução da biodiversidade, uso exacerbado de embalagens, gasto de energia logística de distribuição do mesmo produto em diversas regiões, além da quantidade elevada de calorias, gorduras saturadas, gorduras trans e aditivos químicos, dentre outras razões. </p><p>Uma refeição com alimentos ultraprocessados é provável que tenha uma quantidade elevada de calorias, gorduras saturadas, gorduras trans e aditivos químicos. Neste sentido, quando consumidos em excesso, podem levar a um maior risco de obesidade, doenças cardiovasculares, diabetes tipo 2 e alguns tipos de câncer. As consequências se estendem para a saúde planetária, já que há um uso exacerbado de embalagens elaboradas, gasto de energia em toda a longa cadeia de processamento, logística que percorre longas distâncias, uso de ingredientes associados à emissões de gases de efeito estufa, degradação do solo e escassez de água, dentre outros impactos ambientais. </p><p>Portanto, a Regra de Ouro do Guia Alimentar para a População Brasileira é de "Prefira sempre alimentos in natura ou minimamente processados e preparações culinárias a alimentos ultraprocessados". </p><p>Para saber mais: </p><ul><li><a href="https://www.fsp.usp.br/nupens/a-classificacao-nova/" target="_blank">https://www.fsp.usp.br/nupens/a-classificacao-nova/</a></li><li><a href="https://bvsms.saude.gov.br/bvs/publicacoes/guia_alimentar_populacao_brasileira_2ed.pdf" target="_blank">https://bvsms.saude.gov.br/bvs/publicacoes/guia_alimentar_populacao_brasileira_2ed.pdf</a> </li><li><a href="https://jornal.usp.br/radio-usp/impacto-dos-alimentos-ultraprocessados-tambem-atinge-meio-ambiente/" target="_blank">https://jornal.usp.br/radio-usp/impacto-dos-alimentos-ultraprocessados-tambem-atinge-meio-ambiente/</a></li></ul>'
 		},
 		{
 			'id': 2,
 			'category': '',
-			'title': 'Card 2',
+			'title': 'Restaurante local',
 			'excerpt': 'Some quick example text to build on the card title and make up the bulk of the card’s content.',
-			'image': '',
-			'text': '',
+			'image': 'https://www.daybreakgame.org/assets/dyb_local_project_cards_pt-br_v1.00_03.png',
+			'text': '<p>Um restaurante local é um espaço que valoriza a culinária da sua região, incorporando ingredientes típicos e sazonais que carregam a cultura alimentar local nas preparações que são servidas. Isso reforça a identidade coletiva, promove sustentabilidade e apoia a economia de pequenos produtores.</p><p>Nesse sentido, o consumo de comidas regionais resgata a ligação entre as pessoas e o meio ambiente, já que a oferta de alimentos reflete a ocupação territorial e características do solo e do clima. Além disso, também acontece a valorização do comércio local e da agricultura familiar, fortalecendo a economia da região. Os alimentos regionais e da época também costumam ser mais frescos, saborosos e mais baratos, além de que frequentemente podem ser opções saudáveis para uma alimentação equilibrada.</p><p>Para saber mais: <a href="https://www.fsp.usp.br/sustentarea/wp-content/uploads/2021/12/Revista-12.pdf" target="_blank">https://www.fsp.usp.br/sustentarea/wp-content/uploads/2021/12/Revista-12.pdf</a></p>'
+		},
+		{
+			'id': 3,
+			'category': '',
+			'title': 'Refeição vegana',
+			'excerpt': 'Some quick example text to build on the card title and make up the bulk of the card’s content.',
+			'image': 'https://www.daybreakgame.org/assets/dyb_local_project_cards_pt-br_v1.00_03.png',
+			'text': '<p>Uma refeição vegana é composta por preparações que não utilizam nenhum ingrediente de origem animal, incluindo carnes, peixe, ovos, laticínios e mel, bem como nenhum produto ou insumo utilizado foi testado em animais. O veganismo é um movimento adotado por 7% da população brasileira, enquanto 74% dos brasileiros concordam com a possibilidade de deixar de consumir carne, de acordo com pesquisa Datafolha de março de 2025. </p><p>Substituir produtos de origem animal por vegetais reduz significativamente as emissões de CO₂, uso de terra, água e grãos. Em média, um dia sem produtos animais evita a liberação de 10 kg de CO₂e, economiza 22 m² de terra, 3500 litros de água e 8 kg de grãos que poderiam ser destinados ao consumo humano direto, ao invés de alimentar animais para abate. Tendo isso em vista, criou-se a campanha “Segunda Sem Carne” nos Estados Unidos 2003, que foi implantada no Brasil em 2009 pela Sociedade Vegetariana Brasileira, como uma forma de incentivo à redução do consumo de carne em pelo menos um dia na semana, já que essa pequena atitude tem o potencial de gerar um grande impacto positivo no meio ambiente. </p><p>Para saber mais: </p><ul><li><a href="https://svb.org.br/vegetarianismo-e-veganismo/o-que-e/" target="_blank">https://svb.org.br/vegetarianismo-e-veganismo/o-que-e/</a></li><li><a href="https://svb.org.br/beneficios-ambientais-do-veganismo-no-brasil-o-poder-transformador-de-7-da-populacao/" target="_blank">https://svb.org.br/beneficios-ambientais-do-veganismo-no-brasil-o-poder-transformador-de-7-da-populacao/</a></li></ul>'
+		},
+		{
+			'id': 4,
+			'category': '',
+			'title': 'Refeição escolar',
+			'excerpt': 'Some quick example text to build on the card title and make up the bulk of the card’s content.',
+			'image': 'https://www.daybreakgame.org/assets/dyb_local_project_cards_pt-br_v1.00_03.png',
+			'text': '<p>A refeição escolar é um direito garantido a todos os estudantes da educação básica pública no Brasil, assegurado pela Lei nº 11.947, de 16 de junho de 2009, atualmente regulamentada pela Resolução CD/FNDE nº 06, de 8 de maio de 2020, com suas alterações. Esse direito é viabilizado pelo Programa Nacional de Alimentação Escolar (PNAE), que repassa recursos financeiros federais para oferecer refeições a estudantes de todas as etapas e modalidades da educação básica no período em que estão na escola. O programa atende redes municipais, distrital, estaduais e federal, além de escolas filantrópicas, confessionais sem fins lucrativos e comunitárias conveniadas com estados, municípios ou o Distrito Federal, somando aproximadamente 40 milhões de estudantes contemplados. </p><p>Os cardápios são planejados por nutricionistas, com refeições saudáveis, seguras e culturalmente alinhadas. Além de ter uma atenção direta à Segurança Alimentar dos estudantes, a alimentação escolar promove a valorização de alimentos regionais, ensina sobre hábitos saudáveis desde a infância e estimula a sustentabilidade ao priorizar alimentos in natura. Também, a lei exige que pelo menos 30% dos recursos federais sejam usados para comprar diretamente da agricultura familiar, assim fortalecendo a economia local e a soberania alimentar.</p><p>Para saber mais: <a href="https://www.gov.br/fnde/pt-br/acesso-a-informacao/acoes-e-programas/programas/pnae" target="_blank">https://www.gov.br/fnde/pt-br/acesso-a-informacao/acoes-e-programas/programas/pnae</a></p>'
+		},
+		{
+			'id': 5,
+			'category': '',
+			'title': 'Fast food',
+			'excerpt': 'Some quick example text to build on the card title and make up the bulk of the card’s content.',
+			'image': 'https://www.daybreakgame.org/assets/dyb_local_project_cards_pt-br_v1.00_03.png',
+			'text': '<p>Traduzido para o português "Comida Rápida", é uma refeição preparada, servida e consumida rapidamente. </p><p>No Brasil, o consumo frequente de refeições fora de casa, frequentemente associadas ao <em>fast food</em>, vem aumentando e é monitorado por sistemas como o VIGITEL (Ministério da Saúde) e a Pesquisa de Orçamentos Familiares – POF (IBGE). Esses dados mostram que o aumento da alimentação fora do lar está ligado ao maior consumo de alimentos ultraprocessados e à piora do perfil nutricional da dieta.</p><p>Estudos apontam que a alta ingestão de fast food está associada a obesidade, resistência à insulina, hipertensão e maior risco de doenças cardiovasculares. Isso ocorre devido ao perfil comum entre a maioria dos alimentos classificados como fast food, que são ricos em gorduras saturadas, açúcares, sódio e aditivos químicos, além de que o contexto em que são consumidos estimularem a falta de atenção aos sinais de fome e saciedade. </p><p>Além do impacto individual, o avanço de redes de fast food contribui para a homogeneização de dietas, distanciamento das pessoas com o alimento e perda de preparações culinárias tradicionais. </p><p>Seu impacto ambiental também merece destaque, tendo em vista que as redes de fast food utilizam um grande volume de embalagens descartáveis, contribuem para a perda da biodiversidade e dependem de uma cadeia logística intensiva, na qual os ingredientes precisam ser transportados entre países e algumas etapas do processo são centralizadas em locais específicos, como abatedouros e processadoras. </p><p>Para saber mais:</p><ul><li><a href="https://bvsms.saude.gov.br/bvs/publicacoes/vigitel_brasil_2021.pdf" target="_blank">https://bvsms.saude.gov.br/bvs/publicacoes/vigitel_brasil_2021.pdf</a></li><li><a href="https://pubmed.ncbi.nlm.nih.gov/15639678/" target="_blank">https://pubmed.ncbi.nlm.nih.gov/15639678/</a></li><li><a href="https://static.poder360.com.br/2024/12/pesquisa-de-orcamentos-familiares-2017-2018-ibge.pdf" target="_blank">https://static.poder360.com.br/2024/12/pesquisa-de-orcamentos-familiares-2017-2018-ibge.pdf</a></li></ul>'
 		},
 	];
-
-	const cardsContainer = document.getElementById('cards');
-	const cardTemplate = document.getElementById('card-template');
-	const cardModal = document.getElementById('card-modal');
-	
-	cards.forEach((c) => {
-		let cardNew = cardTemplate.children[0].cloneNode(true);
-		cardNew.querySelector('.card-title').innerHTML = c.title;
-		cardsContainer.appendChild(cardNew);
-	});
 
 	const urlParams = new URLSearchParams(window.location.search);
 	const cardID = urlParams.get('c');
 
-	let cardSelected = cards.find((e) => e.id == cardID);
-	cardModal.querySelector('.card-title').innerHTML = cardSelected.title;
-	cardModal.querySelector('.card-text').innerHTML = cardSelected.excerpt;
+	if (cardID) {
+		let cardData = document.getElementById('card-data');
+		let cardSelected = cards.find((e) => e.id == cardID);
+		
+		cardData.querySelector('.card-title').innerHTML = cardSelected.title;
+		cardData.querySelector('.card-text').innerHTML = cardSelected.text;
+		cardData.querySelector('.card-image').src = cardSelected.image;
+		cardData.classList.remove('d-none');
+	} else {
+		let cardsList = document.getElementById('cards-list');
+		let cardsContainer = document.getElementById('cards');
+		let cardTemplate = document.getElementById('card-template');
+
+		cards.forEach((c) => {
+			let cardNew = cardTemplate.children[0].cloneNode(true);
+			cardNew.querySelector('.card-title').innerHTML = c.title;
+			cardNew.querySelector('.card-text').innerHTML = c.excerpt;
+			cardNew.querySelector('.card-button').href = '?c=' + c.id;
+			cardsContainer.appendChild(cardNew);
+		});
+
+		cardsList.classList.remove('d-none');
+	}
 
 };
